2ff70a2efd9d5657340264ec80a80382b1ab8527
FranciszekMierzejewski <franek.mski@gmail.com>
2026-06-23 21:02:05 +0100

    Added train.py with train_model(), save_model(), load_model() functions

---
+++ src/train.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import joblib
+import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+def train_model(X_train: pd.DataFrame, y_train: pd.DataFrame, C: float = 1.0) -> Pipeline:
+    """
+    Trains logistic regression pipeline on preprocessed training data. C value from grid search found in 05_model_tuning.ipynb
+    """
+    pipeline = Pipeline([
+            ("scalar", StandardScaler()), # standardises features using z = (x - mean)/std to centre data around mean 0
+            ("model", LogisticRegression(class_weight="balanced", max_iter=1000, random_state=42, C=C, penalty="elasticnet", solver="saga"))
+    ])
+    
+    pipeline.fit(X_train, y_train)
+    return pipeline
+
+
+def save_model(pipeline: Pipeline, threshold_recall: float) -> None:
+    """
+    Save fitted pipeline and thresholds
+    """
+    os.makedirs("../models", exist_ok=True)
+
+    joblib.dump(pipeline, "../models/logistic_regression.pkl")
+    joblib.dump(
+        {"recall_threshold": float(threshold_recall)},
+        "../models/thresholds.pkl"
+    )
+
+    print("Model and thresholds saved.")
+
+
+def load_model() -> tuple:
+    """
+    Load pipeline and thresholds
+    """
+    pipeline = joblib.load("../models/logistic_regression.pkl")
+    thresholds = joblib.load("../models/thresholds.pkl")
+
+    return pipeline, thresholds
+
+
+if "__name__" == "__main__":
+    X_train = pd.read_csv("data/X_train.csv")
+    y_train = pd.read_csv("data/y_train.csv").squeeze()
+
+    pipeline = train_model(X_train, y_train)
+
+    save_model(pipeline, threshold_recall = 0.1)
